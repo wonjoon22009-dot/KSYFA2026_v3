7fd8f1e34966ba43906c51cdab6beac43715c79c
wonjoon22009-dot <wonjoon22009@gmail.com>
2026-08-08 19:54:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // publishable key 또는 anon public key만 여기에 넣으세요.
 // service_role key는 절대 이 파일에 넣지 마세요.
 window.KSYFA_CONFIG = {
-  supabaseUrl: "https://xoibhaluljddfitikint.supabase.co/rest/v1/",
+  supabaseUrl: "https://xoibhaluljddfitikint.supabase.co",
   supabaseAnonKey: "sb_publishable_4RAZWbuGKZuKtOG2dL0xHA_z-8Kx6hq"
 };
